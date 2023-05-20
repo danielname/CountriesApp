3c7c16b5fe0ba888dc83a0f6835a59acd5679c16
danielname <daniel.v.name@gmail.com>
2023-05-20 10:38:19 -0500

    New Code - ViewModel Implementation

--- app/src/main/java/com/example/countriesapp/viewmodel/ListViewModel.kt
+++ app/src/main/java/com/example/countriesapp/viewmodel/ListViewModel.kt
@@ -1,4 +1,34 @@
 package com.example.countriesapp.viewmodel
 
-class ListViewModel {
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.countriesapp.model.Country
+
+class ListViewModel: ViewModel() {
+
+    val countries = MutableLiveData<List<Country>>()
+    val countryLoadError = MutableLiveData<Boolean>()
+    val loading = MutableLiveData<Boolean>()
+
+    fun refresh() {
+        fetchCountries()
+    }
+
+    private fun fetchCountries() {
+        val mockData = listOf(Country("CounrtyA"),
+            Country("CounrtyB"),
+            Country("CounrtyC"),
+            Country("CounrtyD"),
+            Country("CounrtyE"),
+            Country("CounrtyF"),
+            Country("CounrtyG"),
+            Country("CounrtyH"),
+            Country("CounrtyI"),
+            Country("CounrtyJ"),
+        )
+
+        countryLoadError.value = false
+        loading.value = false
+        countries.value = mockData
+    }
 }
